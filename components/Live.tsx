8951a6d918d5cc703011793916ac245251e0ac30
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-08-05 17:13:11 -0500

    feat ✨: add comments bubble functionality

--- components/Live.tsx
+++ components/Live.tsx
@@ -11,6 +11,7 @@ import { CursorMode, CursorState, Reaction, ReactionEvent } from '@/types/type';
 import { ReactionSelector } from './reaction/ReactionButton';
 import { FlyingReaction } from './reaction/FlyingReaction';
 import useInterval from '@/hooks/useInterval';
+import { Comments } from './comments/Comments';
 
 type Props = {
   canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
@@ -192,7 +193,7 @@ export const Live = ({ canvasRef }: Props) => {
       onPointerLeave={handlePointerLeave}
       onPointerDown={handlePointerDown}
       onPointerUp={handlePointerUp}
-      className='h-[100vh] w-full flex justify-center items-center text-center'
+      className='relative h-full w-full flex flex-1 justify-center items-center'
     >
       <canvas ref={canvasRef} />
 
@@ -219,15 +220,17 @@ export const Live = ({ canvasRef }: Props) => {
       )}
 
       {cursorState.mode === CursorMode.ReactionSelector && (
-        <ReactionSelector
+        <ReactionSelector 
           setReaction={(reaction) => {
             setReaction(reaction);
-          }}
+          }} 
         />
       )}
 
       {/* Show the live cursors of other users */}
       <LiveCursors others={others} />
+
+      <Comments />
     </div>
   );
 };
